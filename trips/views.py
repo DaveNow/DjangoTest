2c31c0497246649c9920378af75463c3cc27de07
suzilong <18060481475su@gmail.com>
2018-04-26 17:47:28 +0800

    My Django app, first commit

---
+++ trips/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from datetime import datetime
+from .models import Post
+#from django.http import HttpResponse
+
+def home(request):
+    post_list = Post.objects.all()
+    return render(request, 'home.html', {
+        'post_list':post_list,
+})
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+	'current_time': str(datetime.now()),
+   })
+# Create your views here.
