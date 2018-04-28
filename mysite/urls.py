0836de776d9b282708d8e926c0352ead3a1aaf81
suzilong <18060481475su@gmail.com>
2018-04-28 17:51:18 +0800

    2018042805

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from trips import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^hello/$', views.hello_world),
     url(r'^$', views.home)
 ]
